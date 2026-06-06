d7e7d1b790ad74209f50deb51c61d340829bcf58
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-06-06 18:27:31 +0700

    Learn how to implement Link for navigation

--- src/DataLayout.jsx
+++ src/DataLayout.jsx
@@ -1,19 +1,31 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export default function DataLayout() {
     return (
         <>
-        <div>
-            <h1>This is header</h1>
-        </div>
-        <div>
-            <Outlet />
-        </div>
-        <div>
-            <footer>
-                <p>This is footer</p>
-            </footer>
-        </div>
+            <div>
+                <h1>This is header</h1>
+            </div>
+            <div>
+                <ul>
+                    <li><Link to='/data/products'>Products</Link></li>
+                    <li><Link to='/data/customers'>Customers</Link></li>
+                    <li><Link to='/data/sellers'>Sellers</Link></li>
+                    <li><Link to={{
+                        pathname: '/data/products',
+                        search: '?category=shoes',
+                        hash: '#top'
+                    }}>Specified Products</Link></li>
+                </ul>
+            </div>
+            <div>
+                <Outlet />
+            </div>
+            <div>
+                <footer>
+                    <p>This is footer</p>
+                </footer>
+            </div>
         </>
     )
 }

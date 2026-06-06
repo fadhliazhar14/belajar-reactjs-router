7a116d8e28384ec86cb7dbc6556e2554f8bfa38d
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-06-06 18:49:42 +0700

    Learn how to navigating programmatically using useNavigate()'

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router"
+
 export default function Home() {
+    const navigate = useNavigate();
+
+    function handleClick() {
+        navigate({
+            pathname: '/data'
+        });
+    }
+
     return (
         <div>
             <h1>Home page</h1>
             <p>Welcome to home page!</p>
+
+            <button onClick={handleClick}>Go to data page</button>
         </div>
     )
 }

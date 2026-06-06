4be2a438cab277e35ddaf3727446ca2a113c342c
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-06-06 15:29:46 +0700

    Learn how to implement index route

---
+++ src/Data.jsx
@@ -0,0 +1,8 @@
+export default function Data() {
+    return (
+        <div>
+            <h1>Data page</h1>
+            <p>Welcome to data page!</p>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import About from './About.jsx'
 import Product from './Product.jsx'
 import Customer from './Customer.jsx'
 import Seller from './Seller.jsx'
+import Data from './Data.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/data'>
+          <Route index element={<Data />} />
           <Route path='products' element={<Product />} />
           <Route path='customers' element={<Customer />} />
           <Route path='Sellers' element={<Seller />} />

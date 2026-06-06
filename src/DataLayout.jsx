ef5c86c5462a01431689820068e0e0ae3c071282
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-06-06 19:24:22 +0700

    Learn how to use react - useLocation()

--- src/DataLayout.jsx
+++ src/DataLayout.jsx
@@ -1,7 +1,9 @@
-import { Link, NavLink, Outlet } from "react-router";
+import { Link, NavLink, Outlet, useLocation } from "react-router";
 import './data.css'
 
 export default function DataLayout() {
+    const location = useLocation();
+
     return (
         <>
             <div>
@@ -25,6 +27,7 @@ export default function DataLayout() {
             <div>
                 <footer>
                     <p>This is footer</p>
+                    <p>Location : {location.pathname}{location.search}{location.hash}</p>
                 </footer>
             </div>
         </>

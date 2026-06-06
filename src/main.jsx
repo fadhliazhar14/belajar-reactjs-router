bf649ff57ed78239521117b3dbdc4425e4100d30
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-06-06 15:02:06 +0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './Home.jsx'
+import About from './About.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
